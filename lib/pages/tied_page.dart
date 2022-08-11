e498668a4669aad36a1039abbe4ccf5321598ee5
yeferfer <jefer.1142@gmail.com>
2022-08-10 19:11:22 -0500

    tied page frontend

--- lib/pages/tied_page.dart
+++ lib/pages/tied_page.dart
@@ -9,30 +9,42 @@ class TiePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Align(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Empate".toUpperCase(),
-              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Lottie.asset("assets/lottie/tied.json"),
-            ),
-            CupertinoButton(
-              child: const Text("Jugar de Nuevo"),
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const TriquiPage()),
-                );
-              },
-            ),
-          ],
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.white10, Colors.black87, Colors.white12]),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Align(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Empate".toUpperCase(),
+                style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              const SizedBox(height: 20),
+              GestureDetector(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Lottie.asset("assets/lottie/tied.json"),
+                ),
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const TriquiPage()),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
